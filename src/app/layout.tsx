7bcfa2b1aa61d18629114681e92034f34d93dd5e
Willian Gomes <146601824+WillianGomes949@users.noreply.github.com>
2026-01-10 11:00:30 -0300

    aa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata, Viewport } from "next";
-import { Montserrat } from "next/font/google";
-import { Boldonse } from "next/font/google";
+import { Montserrat, Boldonse } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
